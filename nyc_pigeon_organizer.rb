c960c9da53144c8a6bbcba2d3176576daee22d6b
Maxwell Croy <max.croy1@gmail.com>
2020-05-24 01:47:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_results = data.each_with_object({}) do |(key, value), new_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !new_array[name]
+          new_array[name] = {}
+        end
+        if !new_array[name][key]
+          new_array[name][key] = []
+        end
+        if names.include?(name)
+          new_array[name][key] << inner_key.to_s 
+        end
+      end
+    end
+  end
+  final_results
 end
+  
